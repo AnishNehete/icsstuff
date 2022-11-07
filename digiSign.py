239f7dec72c8a6c9078e5c281778ba1864288ad6
Anish Nehete <anishnehete@hotmail.com>
2022-11-07 23:42:29 +0530

    Changes

--- digiSign.py
+++ digiSign.py
@@ -3,16 +3,16 @@ import rsa
 # Implementing helper methods to generate private and public keys
 def generateKeys():
     (publicKey, privateKey) = rsa.newkeys(1024)
-    with open('keys/publicKey.pem', 'wb') as p:
+    with open('publicKey.pem', 'wb') as p:
         p.write(publicKey.save_pkcs1('PEM'))
-    with open('keys/privateKey.pem', 'wb') as p:
+    with open('privateKey.pem', 'wb') as p:
         p.write(privateKey.save_pkcs1('PEM'))
 
 # Implementing method to load the keys
 def loadKeys():
-    with open('keys/publicKey.pem', 'rb') as p:
+    with open('publicKey.pem', 'rb') as p:
         publicKey = rsa.PublicKey.load_pkcs1(p.read())
-    with open('keys/privateKey.pem', 'rb') as p:
+    with open('privateKey.pem', 'rb') as p:
         privateKey = rsa.PrivateKey.load_pkcs1(p.read())
     return privateKey, publicKey
 
